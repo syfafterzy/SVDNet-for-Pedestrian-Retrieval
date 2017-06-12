e7eab4ce5782e98c9597a669585a0a8bc30dc77c
syfafterzy <sunyf15@mails.tsinghua.edu.cn>
2017-06-12 21:33:30 +0800

    first commit

--- SVDNet/caffenet/train_basemodel.sh
+++ SVDNet/caffenet/train_basemodel.sh
@@ -1,9 +1,14 @@
-if [ ! -x "linear" ]
-then
-mkdir linear
-fi
+if [ ! -x "linear" ]; then mkdir linear; fi
 # please define the path of imagenet pre-trained caffenet from caffe rootpath here
+# define your market1501 dataset as well. lmdb is required, or you can change the data format in prototxt files by hand.
 BasemodelPath=
+Train_Data_Path=~/market1501/caffenet_img_train_lmdb
+Val_Data_Path=~/market1501/caffenet_img_test_lmdb
+
+sed -i '16d' train_caffenet_linear.prototxt
+sed -i "16i \    source:\ \"$Train_Data_Path\"" train_caffenet_linear.prototxt
+sed -i '35d' train_caffenet_linear.prototxt
+sed -i "35i \    source:\ \"$Val_Data_Path\"" train_caffenet_linear.prototxt
 cd ../../
 ./build/tools/caffe train -solver solver_linear.prototxt -weights ${BasemodelPath} -gpu 0,1
 mv SVDNet/caffenet/linear/linear_iter_15000.caffemodel SVDNet/caffenet/1024d_linear.caffemodel
--- SVDNet/resnet/train_basemodel.sh
+++ SVDNet/resnet/train_basemodel.sh
@@ -1,9 +1,15 @@
-if [ ! -x "linear" ]
-then
-mkdir linear
-fi
+if [ ! -x "linear" ]; then mkdir linear; fi
 # please define the path of imagenet pre-trained caffenet from caffe rootpath here
+# define path of market-1501 dataset as well; lmdb is required, or you can change the data format in prototxt files by hand
+
 BasemodelPath=
+Train_Data_Path=~/market1501/orig_train_lmdb
+Val_Data_Path=~/market1501/orig_test_lmdb
+
+sed -i '17d' train_resnet_linear.prototxt
+sed -i "17i \    source:\ \"$Train_Data_Path\"" train_resnet_linear.prototxt
+sed -i '35d' train_resnet_linear.prototxt
+sed -i "35i \    source:\ \"$Val_Data_Path\"" train_resnet_linear.prototxt
 cd ../../
 ./build/tools/caffe train -solver solver_linear.prototxt -weights ${BasemodelPath} -gpu 0,1
 mv SVDNet/resnet/linear/linear_iter_25000.caffemodel SVDNet/resnet/1024d_linear.caffemodel
